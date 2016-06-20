11eecefa271bad76cc898c9af34fa3cecf419aae
godoing <20141105063T@mac04.local>
2016-06-20 10:44:55 +0800

    通过视频的学习，了解了最基本的变量与常量的输入方法。还知道了变量可以通过键盘输入改变数值，而常量必须通过代码来确定，不可以手动改动，否则就会出错

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  learn01
+//
+//  Created by 20141105063T on 16/6/20.
+//  Copyright © 2016年 20141105063T. All rights reserved.
+//
+
+import Foundation
+var a = 2
+
+var b = 3
+let  c = a + b
+print (c)
+
+
